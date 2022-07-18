a3c262af66ae03443fb881a2a0755ebabcf382c5
Patrickcabel <pemeka93@gmail.com>
2022-07-18 14:53:36 -0600

    commiting missing files

---
+++ components/index.js
@@ -0,0 +1,16 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+};

b22f34bed80b1e35ed1c5e6bd8554bc307fe5c10
Patrickcabel <pemeka93@gmail.com>
2022-07-18 17:00:48 -0600

    added bids and nft details

--- screens/Details.js
+++ screens/Details.js
@@ -20,7 +20,23 @@ import {
 
 const DetailsHeader = ({ data, navigation }) => (
   <View style={{ width: "100%", height: 373 }}>
-    <Image source={data.image} resizeMode="cover" />
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    />
+
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+    <CircleButton
+      imgUrl={assets.heart}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
   </View>
 );
 
@@ -59,6 +75,25 @@ const Details = ({ route, navigation }) => {
         ListHeaderComponent={() => (
           <React.Fragment>
             <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View
+              style={{
+                padding: SIZES.font,
+              }}
+            >
+              <DetailsDesc data={data} />
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bids
+                </Text>
+              )}
+            </View>
           </React.Fragment>
         )}
       />
